4ab9c3bc3e229cd1226298c525f9d21d0fa9a971
Newbress <75563523+Newbress@users.noreply.github.com>
2026-04-13 11:43:09 +0900

    테스트 부분 수정

--- discodeit/src/test/java/com/sprint/mission/discodeit/storage/s3/S3BinaryContentStorageTest.java
+++ discodeit/src/test/java/com/sprint/mission/discodeit/storage/s3/S3BinaryContentStorageTest.java
@@ -7,9 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@ActiveProfiles("test")
 @SpringBootTest(properties = {
     "discodeit.storage.type=s3",
     "discodeit.storage.s3.access-key=dummy-access-key",
