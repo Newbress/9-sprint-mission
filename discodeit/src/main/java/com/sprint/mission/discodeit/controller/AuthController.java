eb0cfa6911148b6ca893439a437d9e6b2df5e84a
Newbress <75563523+Newbress@users.noreply.github.com>
2026-05-13 17:47:02 +0900

    05. 인증 - 세션을 활용한 현재 사용자 정보 조회

--- discodeit/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,10 +1,14 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.AuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +30,13 @@ public class AuthController {
     String tokenValue = csrfToken.getToken();
     log.debug("CSRF 토큰 요청: {}", tokenValue);
     return ResponseEntity.status(203).build();
+  }
+
+  @GetMapping("/me")
+  public ResponseEntity<UserDto> me(
+      @AuthenticationPrincipal DiscodeitUserDetails userDetails) {
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(userDetails.getUserDto());
   }
 }
