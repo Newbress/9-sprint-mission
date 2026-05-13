d1d52a4d560b514ebd7c84c44b82c3061392cc70
Newbress <75563523+Newbress@users.noreply.github.com>
2026-05-13 11:02:51 +0900

    Spring security 환경 설정 및 CSRF 보호 설정하기

--- discodeit/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +35,12 @@ public class AuthController {
     return ResponseEntity
         .status(HttpStatus.OK)
         .body(user);
+  }
+
+  @GetMapping("/csrf-token")
+  public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
+    String tokenValue = csrfToken.getToken();
+    log.debug("CSRF 토큰 요청: {}", tokenValue);
+    return ResponseEntity.status(203).build();
   }
 }
