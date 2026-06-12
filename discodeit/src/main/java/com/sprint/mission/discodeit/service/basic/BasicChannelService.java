dbed63eb67c99b1521106cbdf5f190d732525518
Newbress <75563523+Newbress@users.noreply.github.com>
2026-06-12 14:41:18 +0900

    private channel 오류 해결

--- discodeit/src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -50,7 +50,6 @@ public class BasicChannelService implements ChannelService {
 
   @Transactional
   @Override
-  @PreAuthorize("hasRole('CHANNEL_MANAGER')")
   public ChannelDto create(PrivateChannelCreateRequest request) {
     log.debug("Private 채널 생성 시작: {}", request);
     Channel channel = new Channel(ChannelType.PRIVATE, null, null);
